ebe7db03177a2f3443ca0bf114f5916196e71688
Patrick Morais <ppternunes@gmail.com>
2022-05-04 09:54:47 -0300

    feat: add FeedBackTypes

---
+++ web/src/helpers/FeedbackTypes.ts
@@ -0,0 +1,27 @@
+import bugImageUrl from '../assets/bug.svg';
+import ideaImageUrl from '../assets/idea.svg';
+import thoughtImageUrl from '../assets/thought.svg';
+
+export const feedbackTypes = {
+  BUG: {
+    title: 'Problema',
+    image: {
+      src: bugImageUrl,
+      alt: 'Imagem de um inseto',
+    },
+  },
+  IDEA: {
+    title: 'Ideia',
+    image: {
+      src: ideaImageUrl,
+      alt: 'Imagem de uma lâmpada',
+    },
+  },
+  OTHER: {
+    title: 'Outro',
+    image: {
+      src: thoughtImageUrl,
+      alt: 'Imagem de um pensamento',
+    },
+  },
+};
