e26e42d2401a1b27f0c3fee7d38bf5c8a252b6f5
Patrick Morais <ppternunes@gmail.com>
2022-05-04 20:43:10 -0300

    feat: create submit form function

--- web/src/components/steps/FeedbackContentStep.tsx
+++ web/src/components/steps/FeedbackContentStep.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { ChangeEvent, FormEvent } from 'react';
 import { ArrowLeft, Camera } from 'phosphor-react';
 import { CloseButton } from '../CloseButton';
 import { FeedbackTypeProps, feedbackTypes } from '../../helpers';
@@ -15,6 +15,14 @@ export function FeedbackContentStep({
 }: FeedbackContentStepProps) {
   const feedbackTypeInfo = feedbackTypes[feedbackType];
   const [screenshot, setScreenshot] = React.useState<string | null>(null);
+  const [commentFeedback, setCommentFeedback] = React.useState('');
+
+  const handleSubmitFeedback = (e: FormEvent) => {
+    e.preventDefault();
+
+    console.log(screenshot);
+    console.log(commentFeedback);
+  };
 
   return (
     <>
@@ -38,18 +46,20 @@ export function FeedbackContentStep({
         <CloseButton />
       </header>
 
-      <form className='my-4 w-full'>
+      <form className='my-4 w-full' onSubmit={handleSubmitFeedback}>
         <textarea
           className='min-w-[304px] w-full min-h-[112px] text-sm placeholder-zinc-400 text-zinc-100 border-2 outline-none border-zinc-600 bg-transparent rounded-md focus:border-brand-500 p-4 resize-none scrollbar scrollbar-thumb-brand-500 scrollbar-track-transparent scrollbar-thin'
           placeholder='Conte com detalhes o que está acontecendo...'
+          onChange={(e) => setCommentFeedback(e.target.value)}
         />
 
         <footer className='flex justify-center mt-2'>
           <CameraButton onScreenshot={setScreenshot} screenshot={screenshot} />
 
           <button
-            type='button'
-            className='bg-brand-500 rounded-md flex-1 border-transparent flex justify-center ml-2 items-center hover:brightness-90 transition duration-300 outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-zinc-900 focus:ring-brand-500'
+            type='submit'
+            className='bg-brand-500 rounded-md flex-1 border-transparent flex justify-center ml-2 items-center hover:brightness-90 transition duration-300 outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-zinc-900 focus:ring-brand-500 disabled:opacity-70 disabled:cursor-not-allowed'
+            disabled={commentFeedback.length === 0}
           >
             Enviar Feedback!
           </button>
