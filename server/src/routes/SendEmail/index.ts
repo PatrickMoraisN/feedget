e81917189835fea6d39c937371d03f6ab314fae0
Patrick Morais <ppternunes@gmail.com>
2022-05-07 18:29:26 -0300

    feat: submit feedback service on route

--- server/src/routes/SendEmail/index.ts
+++ server/src/routes/SendEmail/index.ts
@@ -1,40 +1,26 @@
 import express from 'express';
-import nodemailer from 'nodemailer';
-import { prisma } from '../../prisma';
+import { NodemailerMailAdapter } from '../../adapters/nodemailer/nodemailer-mail-adapter';
+import { PrismaFeedbackRepository } from '../../repositories/prisma/prisma-feedback-repository';
+import { SubmitFeedbackService } from '../../services/SubmitFeedbackService/SubmitFeedbackService';
 
 export const sendEmailRoute = express.Router();
-
-const transport = nodemailer.createTransport({
-  host: 'smtp.mailtrap.io',
-  port: 2525,
-  auth: {
-    user: '0ba5ea66b6f0e2',
-    pass: '039da0d7bcefa5',
-  },
-});
 
 sendEmailRoute.post('/feedback', async (req, res) => {
   const { type, comment, screenshot } = req.body;
 
-  const feedback = await prisma.feedBack.create({
-    data: {
-      type,
-      comment,
-      screenshot,
-    },
+  const prismaFeedbackRepository = new PrismaFeedbackRepository();
+  const nodemailerMailAdapter = new NodemailerMailAdapter();
+
+  const submitFeedbackService = new SubmitFeedbackService(
+    prismaFeedbackRepository,
+    nodemailerMailAdapter
+  );
+
+  await submitFeedbackService.execute({
+    type,
+    comment,
+    screenshot,
   });
 
-  transport.sendMail({
-    from: 'Equipe Feedget <oi@feedget.com>',
-    to: 'Patrick Morais <patrick.nunes@remessaonline.com.br>',
-    subject: 'Novo Feedback',
-    html: [
-      '<div style="font-family: sans-serif; font-size: 16px; color: #222;">',
-      `<p>Tipo do feedback: ${type}</p>`,
-      `<p>Comentário: ${comment}</p>`,
-      '</div>',
-    ].join('\n'),
-  });
-
-  return res.status(201).json({ data: feedback });
+  return res.status(201).json({ data: 'Feedback sent!' });
 });
