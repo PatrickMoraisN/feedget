b16bf49b4f7f323570e9819d274d2e5c287b3071
Patrick Morais <ppternunes@gmail.com>
2022-05-09 00:27:39 -0300

    feat: add cors and update express limit

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,9 +1,12 @@
 import express from 'express';
+import cors from 'cors';
 import { sendEmailRoute } from './routes/SendEmail';
 
 const app = express();
 
-app.use(express.json());
+app.use(cors());
+app.use(express.json({ limit: '10mb' }));
+app.use(express.urlencoded({ limit: '10mb' }));
 app.use(sendEmailRoute);
 
 app.listen(3333, () =>
